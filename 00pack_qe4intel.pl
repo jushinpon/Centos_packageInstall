75b8dc6f197b9f1169d35e1ec9d2fc95911b6e37
jushinpon <jushinpon@gmail.com>
2020-06-29 00:04:34 +0800

    modify interface layer to 32

--- 00pack_qe4intel.pl
+++ 00pack_qe4intel.pl
@@ -21,30 +21,33 @@ my $URL = "https://github.com/QEF/q-e.git";#url to download
 my $Dir4download = "qe_download"; #the directory we download Mpich
 
 my $script_CurrentPath = getcwd; #get perl code path
-system("rm -rf $Dir4download");# remove the older directory first
-system("mkdir $Dir4download");# make a directory in current path
-
-#download qe
-chdir("$script_CurrentPath/$Dir4download");# cd to this dir for downloading the packages
-#get the latest package in the directory and save it as the filename you want
-system("git clone $URL");  
-print $Check "$?:wget -O or git clone qe $URL\n";
-
+#system("rm -rf $Dir4download");# remove the older directory first
+#system("mkdir $Dir4download");# make a directory in current path
+#
+##download qe
+#chdir("$script_CurrentPath/$Dir4download");# cd to this dir for downloading the packages
+##get the latest package in the directory and save it as the filename you want
+#system("git clone $URL");  
+#print $Check "$?:wget -O or git clone qe $URL\n";
+#
 chdir("$script_CurrentPath/$Dir4download/q-e");
-
-my $BLAS_LIBS="BLAS_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_ilp64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl\"";
-my $LAPACK_LIBS="LAPACK_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_ilp64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl\"";
-my $SCALAPACK_LIBS="SCALAPACK_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_ilp64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl\"";
-my $FFT_LIBS="FFT_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_ilp64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl\"";
+my $prefix = "--prefix=/opt/QE_PSXE2020";
+system ("rm -rf /opt/QE_PSXE2020");# remove the older directory first
+#print "";
+my $BLAS_LIBS="BLAS_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_lp64 -lpthread -lm -ldl\"";
+my $LAPACK_LIBS="LAPACK_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_lp64 -lpthread -lm -ldl\"";
+my $SCALAPACK_LIBS="SCALAPACK_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_lp64 -lpthread -lm -ldl\"";
+my $FFT_LIBS="FFT_LIBS=\"-L\$\{MKLROOT\}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_lp64 -lpthread -lm -ldl\"";
 my $FFLAGS="FFLAGS=\"-O3 -assume byterecl -g -traceback\"";
 my $MPI_LIBS ="MPI_LIBS=\"-L/home/intel_compiler/impi/2019.7.217/intel64/lib -lmpi\"";#### need to use your own path for impi
 
-system("./configure F90=ifort F77=mpiifort MPIF90=mpiifort CC=mpiicc $BLAS_LIBS $LAPACK_LIBS $SCALAPACK_LIBS $FFT_LIBS $FFLAGS $MPI_LIBS --enable-parallel");
+system("./configure $prefix F90=ifort F77=mpiifort MPIF90=mpiifort CC=mpiicc $BLAS_LIBS $LAPACK_LIBS $SCALAPACK_LIBS $FFT_LIBS $FFLAGS $MPI_LIBS --enable-parallel");
 if($? != 0 ){die "**QE configure fails";}
 #after the configure process is done, type "make" and then "make install"
 system("make clean"); 
-system("make pw -j $core4make"); 
 if($? != 0 ){die "**make QE fails";}
+system("make pw -j $core4make"); 
 print $Check "$?: make qe\n";
+system("make install"); 
 print $Check "ALL DONE!\n";
 close($Check);	#
